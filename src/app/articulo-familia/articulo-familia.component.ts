7fac31d1a014415c1b5cdca974ed6ac720da1271
ant-villasante98 <hilares33v@gmail.com>
2021-06-08 00:27:43 -0300

    1 file modified, 1 added

--- src/app/articulo-familia/articulo-familia.component.ts
+++ src/app/articulo-familia/articulo-familia.component.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-articulo-familia',
-  templateUrl: './articulo-familia.component.html',
+  templateUrl: './articulo-familia.html',
   styleUrls: ['./articulo-familia.component.css']
 })
 export class ArticuloFamiliaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
