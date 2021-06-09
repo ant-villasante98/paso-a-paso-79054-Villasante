ff4c6ec013b6db478e3d5d329d08003aea9769ab
ant-villasante98 <hilares33v@gmail.com>
2021-06-09 19:13:23 -0300

    1 file modified

--- src/app/services/s-articulo-familia.service.ts
+++ src/app/services/s-articulo-familia.service.ts
@@ -5,8 +5,7 @@ export class SArticuloFamiliaService {
   resourceURL: string;
 
   constructor(private httpclient: HttpClient) {
-    this.resourceURL =
-      'https://labsys.frc.utn.edu.ar:8443/api/ArticulosFamilias';
+    this.resourceURL = 'https://localhost:44320/api/ArticulosFamilias';
   }
   get() {
     return this.httpclient.get(this.resourceURL);
